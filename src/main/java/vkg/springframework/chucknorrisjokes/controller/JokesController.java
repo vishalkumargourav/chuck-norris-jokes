9e619bb89263a20679edc30f4ac5f6d8f0a7cb8c
vishalkumargourav <vishalkumargourav@github.com>
2022-10-28 17:37:05 +0530

    Improved design by using service interface in controller

--- src/main/java/vkg/springframework/chucknorrisjokes/controller/JokesController.java
+++ src/main/java/vkg/springframework/chucknorrisjokes/controller/JokesController.java
@@ -3,19 +3,20 @@ package vkg.springframework.chucknorrisjokes.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vkg.springframework.chucknorrisjokes.services.JokeService;
 import vkg.springframework.chucknorrisjokes.services.JokeServiceImpl;
 
 @Controller
 public class JokesController {
-    private final JokeServiceImpl jokeServiceImpl;
+    private final JokeService jokeService;
 
-    public JokesController(JokeServiceImpl jokeServiceImpl) {
-        this.jokeServiceImpl = jokeServiceImpl;
+    public JokesController(JokeService jokeServiceImpl) {
+        this.jokeService = jokeServiceImpl;
     }
 
     @RequestMapping("/getJoke")
     public String getChuckJokes(Model model){
-        model.addAttribute("joke", jokeServiceImpl.getChuckNorrisQuotes());
+        model.addAttribute("joke", jokeService.getChuckNorrisQuotes());
 
         return "jokes/jokeTemplate";
     }
